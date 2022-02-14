436f5f003ee93f8cd231c145023152ca5e58f811
michi <michellewong1998@gmail.com>
2022-02-13 23:24:55 -0500

    added pseudocode

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+        // Record user's input on submit (event listener)
+        // Create a URL object with specified params (e.g. query fish, query bugs, query sea creatures, etc.)
+        // Fetch object, parse into JSON, this will return an object of objects (item)
+        // Then use a for-each loop to iterate over each item and its properties, save all of these individual objects to an array
+        // Use a filter method to take only the objects which have an availability (month-array-northern and month-array-southern) matching the user's specified month
+        // Create a "li" element to display each "item" as a card using the following properties:
+        // item.title, item.museum_fact, item.location, etc...
+        // Add the information above to the li
+        // Append the li to the end of the ul which will host each of the fish, sea creatures, and bug sections.
